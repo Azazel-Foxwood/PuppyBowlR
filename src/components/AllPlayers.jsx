8cf5f09da5de250c2d76b8bd5d10d93af057dd56
Azazel-Foxwood <redseabass95@gmail.com>
2023-07-31 21:42:49 -0500

    tring to do single player

--- src/components/AllPlayers.jsx
+++ src/components/AllPlayers.jsx
@@ -1,9 +1,15 @@
 import {useState, useEffect} from 'react'
 import { allPlayers } from '../API'
+// import { useNavigate } from 'react-router-dom';
+import SinglePlayer from './SinglePlayer';
 
 export default function AllPlayers() {
     const [players, setPlayers] = useState([])
 
+    // function handleClick(){
+        
+    //     console.log('click')
+    // }
     useEffect(() => {
         async function getAllPlayers () {
                 const response = await allPlayers();
@@ -12,14 +18,19 @@ export default function AllPlayers() {
         }
         getAllPlayers();
     }, []); 
-
+    // const navigate = useNavigate();
     return (
         <>
             {players.map((all, id) =>(
-                <div key={id}>{all.name}
-                {/* <img src ={all.imageUrl}></img> */}
+                <div key={id}>
+                    <h1>
+                        {all.name}
+                        </h1>
+                <img style={{height: "150px"}} src ={all.imageUrl}></img>
+                <button onClick={() => {<SinglePlayer/>}}>see more</button>
                 </div>
             ))}
+            {/* <button onClick={() => navigate(`/${all.id}`)}>see more</button> */}
             <h1>🥲</h1>
         </>
     )
---
+++ src/components/SinglePlayer.jsx
@@ -0,0 +1,12 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import {singlePlayer} from '../API';
+
+export default function SinglePlayer(){
+
+    return (
+        <>
+        {console.log('test')}
+        </>
+    )
+}
