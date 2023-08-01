d55505240bd13f45a726bc40fe3227a43b19776d
Azazel-Foxwood <redseabass95@gmail.com>
2023-07-31 20:40:16 -0500

    all players now render

--- src/components/AllPlayers.jsx
+++ src/components/AllPlayers.jsx
@@ -2,32 +2,25 @@ import {useState, useEffect} from 'react'
 import { allPlayers } from '../API'
 
 export default function AllPlayers() {
-    const [players, setPlayers] = useState('')
+    const [players, setPlayers] = useState([])
 
     useEffect(() => {
         async function getAllPlayers () {
-            
-            try {
-                
-                const players = await allPlayers();
-                console.log(players);
-                setPlayers(players);
-                
-            } catch(error) {
-                console.error(error);
-            }
+                const response = await allPlayers();
+                console.log(response);
+                setPlayers(response);
         }
         getAllPlayers();
-    }); 
+    }, []); 
 
     return (
         <>
-            {/* {players.map((allPlayer) =>{
-                return (
-                    {allPlayer}
-                )
-            })} */}
-
+            {players.map((all, id) =>(
+                <div key={id}>{all.name}
+                {/* <img src ={all.imageUrl}></img> */}
+                </div>
+            ))}
+            <h1>🥲</h1>
         </>
     )
 }
