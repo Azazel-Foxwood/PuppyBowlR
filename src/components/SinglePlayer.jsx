8982f55268a8cd0b2ab77d88ff3613819d5e44bc
Azazel-Foxwood <redseabass95@gmail.com>
2023-07-31 22:44:24 -0500

    single player renders

--- src/components/SinglePlayer.jsx
+++ src/components/SinglePlayer.jsx
@@ -3,10 +3,26 @@ import { useEffect, useState } from "react";
 import {singlePlayer} from '../API';
 
 export default function SinglePlayer(){
+    const { id } = useParams();
 
+    const [player, setPlayer] = useState([])
+    useEffect(() => {
+        async function getSinglePlayer () {
+                const response = await singlePlayer(id);
+                console.log(response);
+                setPlayer(response);
+                // (player.team.name ? player.team.name : '')
+        }
+        getSinglePlayer();
+    }, []);
     return (
         <>
-        {console.log('test')}
+        
+        <h1>{player.name}</h1>
+        <img style={{height: "150px"}} src= {player.imageUrl}/>
+        <h2>{player.breed}</h2>
+        {player.status}<br/>
+        {/* {player.team.name}; */}
         </>
     )
 }
